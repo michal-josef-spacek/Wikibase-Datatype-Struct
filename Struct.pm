8c92ca4cd4df7acf87006a161f45e12e0ef6c6ea
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-11-13 20:37:18 +0100

    First version of Struct documentation

--- Struct.pm
+++ Struct.pm
@@ -8,3 +8,96 @@ our $VERSION = 0.01;
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Wikidata::Datatype::Struct - Wikidata structure serialization.
+
+=head1 DESCRIPTION
+
+This conversion is between objects defined in Wikidata::Datatype and structures
+serialized via JSON to MediaWiki.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Wikidata::Datatype::Struct::Reference>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Sitelink>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Snak>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Statement>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Utils>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Value>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Value::Globecoordinate>
+
+Wikidata globe coordinate structure serialization.
+
+=item L<Wikidata::Datatype::Struct::Value::Item>
+
+Wikidata item structure serialization.
+
+=item L<Wikidata::Datatype::Struct::Value::Monolingual>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Value::Property>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Value::Quantity>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Value::String>
+
+TODO
+
+=item L<Wikidata::Datatype::Struct::Value::Time>
+
+TODO
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Wikidata-Datatype-Struct>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© Michal Josef Špaček 2020
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
