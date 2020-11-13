fac1b4c82d73e660ae9986f092efd61d3c3394d5
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-11-13 20:28:59 +0100

    Struct::Value::Globecoordinate: Add doc

--- Struct/Value/Globecoordinate.pm
+++ Struct/Value/Globecoordinate.pm
@@ -64,3 +64,180 @@ sub struct2obj {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Wikidata::Datatype::Struct::Value::Globecoordinate - Wikidata globe coordinate structure serialization.
+
+=head1 SYNOPSIS
+
+ use Wikidata::Datatype::Struct::Value::Globecoordinate qw(obj2struct struct2obj);
+
+ my $struct_hr = obj2struct($obj, $base_uri);
+ my $obj = struct2obj($struct_hr);
+
+=head1 DESCRIPTION
+
+This conversion is between objects defined in Wikidata::Datatype and structures
+serialized via JSON to MediaWiki.
+
+=head1 SUBROUTINES
+
+=head2 C<obj2struct>
+
+ my $struct_hr = obj2struct($obj, $base_uri);
+
+Convert Wikidata::Datatype::Value::Globecoordinate instance to structure.
+
+C<$base_uri> is base URL of Wikibase/Wikidata system (e.g. http://test.wikidata.org/entity/).
+
+Returns reference to hash with structure.
+
+=head2 C<struct2obj>
+
+ my $obj = struct2obj($struct_hr);
+
+Convert structure of item to object.
+
+Returns Wikidata::Datatype::Value::Globecoordinate istance.
+
+=head1 ERRORS
+
+ obj2struct():
+         Object isn't 'Wikidata::Datatype::Value::Globecoordinate'.
+
+ struct2obj():
+         Structure isn't for 'globecoordinate' datatype.
+
+=head1 EXAMPLE1
+
+ use strict;
+ use warnings;
+
+ use Data::Printer;
+ use Wikidata::Datatype::Value::Globecoordinate;
+ use Wikidata::Datatype::Struct::Value::Globecoordinate qw(obj2struct);
+
+ # Object.
+ my $obj = Wikidata::Datatype::Value::Globecoordinate->new(
+         'value' => [49.6398383, 18.1484031],
+ );
+
+ # Get structure.
+ my $struct_hr = obj2struct($obj, 'http://test.wikidata.org/entity/');
+
+ # Dump to output.
+ p $struct_hr;
+
+ # Output:
+ # \ {
+ #     type    "globecoordinate",
+ #     value   {
+ #         altitude    "null",
+ #         globe       "http://test.wikidata.org/entity/Q2",
+ #         latitude    49.6398383,
+ #         longitude   18.1484031,
+ #         precision   1e-07
+ #     }
+ # }
+
+=head1 EXAMPLE2
+
+ use strict;
+ use warnings;
+
+ use Data::Printer;
+ use Wikidata::Datatype::Struct::Value::Globecoordinate qw(struct2obj);
+
+ # Item structure.
+ my $struct_hr = {
+         'type' => 'globecoordinate',
+         'value' => {
+                 'altitude' => 'null',
+                 'globe' => 'http://test.wikidata.org/entity/Q2',
+                 'latitude' => 49.6398383,
+                 'longitude' => 18.1484031,
+                 'precision' => 1e-07,
+         },
+ };
+
+ # Get object.
+ my $obj = struct2obj($struct_hr);
+
+ # Get globe.
+ my $globe = $obj->globe;
+
+ # Get longitude.
+ my $longitude = $obj->longitude;
+
+ # Get latitude.
+ my $latitude = $obj->latitude;
+
+ # Get precision.
+ my $precision = $obj->precision;
+
+ # Get type.
+ my $type = $obj->type;
+
+ # Get value.
+ my $value_ar = $obj->value;
+
+ # Print out.
+ print "Globe: $globe\n";
+ print "Latitude: $latitude\n";
+ print "Longitude: $longitude\n";
+ print "Precision: $precision\n";
+ print "Type: $type\n";
+ print 'Value: '.(join ', ', @{$value_ar})."\n";
+
+ # Output:
+ # Globe: Q2
+ # Latitude: 49.6398383
+ # Longitude: 18.1484031
+ # Precision: 1e-07
+ # Type: globecoordinate
+ # Value: 49.6398383, 18.1484031
+
+=head1 DEPENDENCIES
+
+L<Error::Pure>,
+L<Exporter>,
+L<Readonly>,
+L<URI>,
+L<Wikidata::Datatype::Value::Globecoordinate>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Wikidata::Datatype::Struct>
+
+TODO
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Wikidata-Datatype-Struct>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© Michal Josef Špaček 2020
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
