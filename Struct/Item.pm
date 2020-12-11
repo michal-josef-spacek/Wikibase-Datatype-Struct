de0af307220858489ed2ce94b3071900d9ac2ee5
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-12-11 15:19:26 +0100

    Implement Item::struct2obj()

--- Struct/Item.pm
+++ Struct/Item.pm
@@ -104,17 +104,62 @@ sub struct2obj {
 		err "Structure isn't for 'item' type.";
 	}
 
-	# TODO
+	# Aliases.
+	my $aliases_ar = [];
+	foreach my $lang (keys %{$struct_hr->{'aliases'}}) {
+		foreach my $alias_hr (@{$struct_hr->{'aliases'}->{$lang}}) {
+			push @{$aliases_ar}, Wikibase::Datatype::Struct::Language::struct2obj(
+				$alias_hr,
+			);
+		}
+	}
+
+	# Descriptions.
+	my $descriptions_ar = [];
+	foreach my $lang (keys %{$struct_hr->{'descriptions'}}) {
+		push @{$descriptions_ar}, Wikibase::Datatype::Struct::Language::struct2obj(
+			$struct_hr->{'descriptions'}->{$lang},
+		);
+	}
+
+	# Labels.
+	my $labels_ar = [];
+	foreach my $lang (keys %{$struct_hr->{'labels'}}) {
+		push @{$labels_ar}, Wikibase::Datatype::Struct::Language::struct2obj(
+			$struct_hr->{'labels'}->{$lang},
+		);
+	}
+
+	# Sitelinks.
+	my $sitelinks_ar = [];
+	foreach my $site (keys %{$struct_hr->{'sitelinks'}}) {
+		push @{$sitelinks_ar}, Wikibase::Datatype::Struct::Sitelink::struct2obj(
+			$struct_hr->{'sitelinks'}->{$site},
+		);
+	}
+
+	# Statements.
+	my $statements_ar = [];
+	foreach my $property (keys %{$struct_hr->{'claims'}}) {
+		foreach my $claim_hr (@{$struct_hr->{'claims'}->{$property}}) {
+			push @{$statements_ar}, Wikibase::Datatype::Struct::Statement::struct2obj(
+				$claim_hr,
+			);
+		}
+	}
+
 	my $obj = Wikibase::Datatype::Item->new(
-		'aliases' => [],
-		'descriptions' => [],
-		'labels' => [],
-		'lastrevid' => '',
-		'modified' => '',
-		'ns' => '',
-		'sitelinks' => [],
-		'statements' => [],
-		'title' => '',
+		'aliases' => $aliases_ar,
+		'descriptions' => $descriptions_ar,
+		'id' => $struct_hr->{'id'},
+		'labels' => $labels_ar,
+		'lastrevid' => $struct_hr->{'lastrevid'},
+		'modified' => $struct_hr->{'modified'},
+		'ns' => $struct_hr->{'ns'},
+		'page_id' => $struct_hr->{'pageid'},
+		'sitelinks' => $sitelinks_ar,
+		'statements' => $statements_ar,
+		'title' => $struct_hr->{'title'},
 	);
 
 	return $obj;
@@ -601,20 +646,143 @@ Returns Wikibase::Datatype::Item instance.
  use strict;
  use warnings;
 
+ use Data::Printer;
  use Wikibase::Datatype::Struct::Item qw(struct2obj);
 
  # Item structure.
  my $struct_hr = {
- # TODO
+         'aliases' => {
+                 'en' => [{
+                         'language' => 'en',
+                         'value' => 'Douglas Noel Adams',
+                 }, {
+                         'language' => 'en',
+                         'value' => 'Douglas Noël Adams',
+                 }],
+                 'cs' => [{
+                         'language' => 'cs',
+                         'value' => 'Douglas Noel Adams',
+                 }, {
+                         'language' => 'cs',
+                         'value' => 'Douglas Noël Adams',
+                 }],
+         },
+         'claims' => {
+                 'P394' => [{
+                         'rank' => 'normal',
+                         'id' => 'Q42$c763016e-49e0-89b9-f717-2b18af5148f9',
+                         'references' => [{
+                                 'hash' => 'ed9d0472fca124cea519c0a37eba5f33f10baa22',
+                                 'snaks' => {
+                                         'P1943' => [{
+                                                 'datavalue' => {
+                                                         'type' => 'string',
+                                                         'value' => 'http://wikipedia.org/',
+                                                 },
+                                                 'datatype' => 'url',
+                                                 'snaktype' => 'value',
+                                                 'hash' => 'b808d4d54bed4daf07d9ac73353c0d1173cfa3c0',
+                                                 'property' => 'P1943',
+                                         }],
+                                 },
+                                 'snaks-order' => [
+                                         'P1943',
+                                 ],
+                         }],
+                         'type' => 'statement',
+                         'mainsnak' => {
+                                 'datatype' => 'quantity',
+                                 'datavalue' => {
+                                         'type' => 'quantity',
+                                         'value' => {
+                                                 'amount' => '+0.00000000000000000000000000000091093821500',
+                                                 'upperBound' => '+0.00000000000000000000000000000091093821545',
+                                                 'lowerBound' => '+0.00000000000000000000000000000091093821455',
+                                                 'unit' => 'http://test.wikidata.org/entity/Q213',
+                                         },
+                                 },
+                                 'snaktype' => 'value',
+                                 'hash' => 'fac57bc5b94714fb2390cce90f58b6a6cf9b9717',
+                                 'property' => 'P394',
+                         },
+                 }],
+         },
+         'descriptions' => {
+                 'en' => {
+                         'language' => 'en',
+                         'value' => 'human',
+                 },
+                 'cs' => {
+                         'language' => 'cs',
+                         'value' => 'člověk',
+                 },
+         },
+         'id' => 'Q42',
+         'labels' => {
+                 'en' => {
+                         'language' => 'en',
+                         'value' => 'Douglas Adams',
+                 },
+                 'cs' => {
+                         'language' => 'cs',
+                         'value' => 'Douglas Adams',
+                 },
+         },
+         'lastrevid' => 534820,
+         'modified' => '2020-12-02T13:39:18Z',
+         'ns' => 0,
+         'pageid' => '703',
+         'sitelinks' => {
+                 'cswiki' => {
+                         'title' => 'Douglas Adams',
+                         'badges' => [],
+                         'site' => 'cswiki',
+                 },
+         },
+         'type' => 'item',
+         'title' => 'Q42',
  };
 
  # Get object.
  my $obj = struct2obj($struct_hr);
 
- # TODO
+ # Print out.
+ p $obj;
 
  # Output:
- # TODO
+ # Wikibase::Datatype::Item  {
+ #     Parents       Mo::Object
+ #     public methods (9) : BUILD, can (UNIVERSAL), DOES (UNIVERSAL), err (Error::Pure), check_array_object (Mo::utils), check_number (Mo::utils), check_number_of_items (Mo::utils), isa (UNIVERSAL), VERSION (UNIVERSAL)
+ #     private methods (1) : __ANON__ (Mo::build)
+ #     internals: {
+ #         aliases        [
+ #             [0] Wikibase::Datatype::Value::Monolingual,
+ #             [1] Wikibase::Datatype::Value::Monolingual,
+ #             [2] Wikibase::Datatype::Value::Monolingual,
+ #             [3] Wikibase::Datatype::Value::Monolingual
+ #         ],
+ #         descriptions   [
+ #             [0] Wikibase::Datatype::Value::Monolingual,
+ #             [1] Wikibase::Datatype::Value::Monolingual
+ #         ],
+ #         id             "Q42",
+ #         labels         [
+ #             [0] Wikibase::Datatype::Value::Monolingual,
+ #             [1] Wikibase::Datatype::Value::Monolingual
+ #         ],
+ #         lastrevid      534820,
+ #         modified       "2020-12-02T13:39:18Z",
+ #         ns             0,
+ #         page_id        703,
+ #         sitelinks      [
+ #             [0] Wikibase::Datatype::Sitelink
+ #         ],
+ #         statements     [
+ #             [0] Wikibase::Datatype::Statement
+ #         ],
+ #         title          "Q42"
+ #     }
+ # }
 
 =head1 DEPENDENCIES
 
