6bbaaa0548e162fd54486ed069af93bb2745247b
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2021-01-27 01:44:06 +0100

    Prepare for 0.08 version

--- Struct.pm
+++ Struct.pm
@@ -3,7 +3,7 @@ package Wikibase::Datatype::Struct;
 use strict;
 use warnings;
 
-our $VERSION = 0.07;
+our $VERSION = 0.08;
 
 1;
 
@@ -110,6 +110,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.07
+0.08
 
 =cut
