a127b3e3c9704842b7f260750321d41ed3283a52
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-07-09 16:48:41 +0200

    Fix Lexeme struct2obj(), was very bad

--- examples/ex20.pl
+++ examples/ex20.pl
@@ -4,45 +4,166 @@ use strict;
 use warnings;
 
 use Data::Printer;
+use Unicode::UTF8 qw(decode_utf8);
 use Wikibase::Datatype::Struct::Lexeme qw(struct2obj);
 
 # Lexeme structure.
 my $struct_hr = {
-        'grammaticalFeatures' => [
-                'Q163012',
-                'Q163014',
-        ],
-        'representations' => {
-                'cs' => {
-                        'language' => 'cs',
-                        'value' => 'Representation cs',
-                },
-                'en' => {
-                        'language' => 'en',
-                        'value' => 'Representation en',
-                },
-        },
         'claims' => {
-                'P31' => [{
+                'P5185' => [{
                         'mainsnak' => {
-                                'datatype' => 'wikibase-item',
                                 'datavalue' => {
                                         'type' => 'wikibase-entityid',
                                         'value' => {
                                                 'entity-type' => 'item',
-                                                'id' => 'Q5',
-                                                'numeric-id' => 5,
+                                                'id' => 'Q499327',
+                                                'numeric-id' => 499327,
                                         },
                                 },
-                                'property' => 'P31',
+                                'datatype' => 'wikibase-item',
+                                'property' => 'P5185',
                                 'snaktype' => 'value',
                         },
                         'rank' => 'normal',
+                        'references' => [{
+                                'snaks' => {
+                                        'P214' => [{
+                                                'datavalue' => {
+                                                        'type' => 'string',
+                                                        'value' => '113230702',
+                                                },
+                                                'datatype' => 'external-id',
+                                                'property' => 'P214',
+                                                'snaktype' => 'value',
+                                        }],
+                                        'P248' => [{
+                                                'datavalue' => {
+                                                        'type' => 'wikibase-entityid',
+                                                        'value' => {
+                                                                'entity-type' => 'item',
+                                                                'id' => 'Q53919',
+                                                                'numeric-id' => 53919,
+                                                        },
+                                                },
+                                                'datatype' => 'wikibase-item',
+                                                'property' => 'P248',
+                                                'snaktype' => 'value',
+                                        }],
+                                        'P813' => [{
+                                                'datavalue' => {
+                                                        'type' => 'time',
+                                                        'value' => {
+                                                                'after' => 0,
+                                                                'before' => 0,
+                                                                'calendarmodel' => 'http://test.wikidata.org/entity/Q1985727',
+                                                                'precision' => 11,
+                                                                'time' => '+2013-12-07T00:00:00Z',
+                                                                'timezone' => 0,
+                                                        },
+                                                },
+                                                'datatype' => 'time',
+                                                'property' => 'P813',
+                                                'snaktype' => 'value',
+                                        }],
+                                },
+                                'snaks-order' => [
+                                        'P248',
+                                        'P214',
+                                        'P813',
+                                ],
+                        }],
                         'type' => 'statement',
                 }],
         },
+        'forms' => [{
+                'claims' => {
+                        'P898' => [{
+                                'mainsnak' => {
+                                        'datavalue' => {
+                                                'type' => 'string',
+                                                'value' => decode_utf8('pɛs'),
+                                        },
+                                        'datatype' => 'string',
+                                        'property' => 'P898',
+                                        'snaktype' => 'value',
+                                },
+                                'rank' => 'normal',
+                                'type' => 'statement',
+                        }],
+                },
+                'grammaticalFeatures' => [
+                        'Q110786',
+                        'Q131105',
+                ],
+                'id' => 'L469-F1',
+                'representations' => {
+                        'cs' => {
+                                'language' => 'cs',
+                                'value' => 'pes',
+                        },
+                },
+        }],
+        'id' => 'L469',
+        'language' => 'Q9056',
+        'lastrevid' => 1428556087,
+        'lemmas' => {
+                'cs' => {
+                        'language' => 'cs',
+                        'value' => 'pes',
+                },
+        },
+        'lexicalCategory' => 'Q1084',
+        'modified' => '2022-06-24T12:42:10Z',
+        'ns' => 146,
+        'pageid' => 54393954,
+        'senses' => [{
+                'claims' => {
+                        'P18' => [{
+                                'mainsnak' => {
+                                        'datavalue' => {
+                                                'type' => 'string',
+                                                'value' => 'Canadian Inuit Dog.jpg',
+                                        },
+                                        'datatype' => 'commonsMedia',
+                                        'property' => 'P18',
+                                        'snaktype' => 'value',
+                                },
+                                'rank' => 'normal',
+                                'type' => 'statement',
+                        }],
+                        'P5137' => [{
+                                'mainsnak' => {
+                                        'datavalue' => {
+                                                'type' => 'wikibase-entityid',
+                                                'value' => {
+                                                        'entity-type' => 'item',
+                                                        'id' => 'Q144',
+                                                        'numeric-id' => 144,
+                                                },
+                                        },
+                                        'datatype' => 'wikibase-item',
+                                        'property' => 'P5137',
+                                        'snaktype' => 'value',
+                                },
+                                'rank' => 'normal',
+                                'type' => 'statement',
+                        }],
+                },
+                'glosses' => {
+                        'cs' => {
+                                'language' => 'cs',
+                                'value' => decode_utf8('psovitá šelma chovaná jako domácí zvíře'),
+                        },
+                        'en' => {
+                                'language' => 'en',
+                                'value' => 'domesticated mammal related to the wolf',
+                        },
+                },
+                'id' => 'L469-S1',
+        }],
+        'title' => 'Lexeme:L469',
         'type' => 'lexeme',
-};
+ };
 
 # Get object.
 my $obj = struct2obj($struct_hr);
@@ -52,21 +173,36 @@ p $obj;
 
 # Output:
 # Wikibase::Datatype::Lexeme  {
-#     Parents       Mo::Object
-#     public methods (8) : BUILD, can (UNIVERSAL), DOES (UNIVERSAL), err (Error::Pure), check_array_object (Mo::utils), check_entity (Wikibase::Datatype::Utils), isa (UNIVERSAL), VERSION (UNIVERSAL)
-#     private methods (1) : __ANON__ (Mo::is)
+#     parents: Mo::Object
+#     public methods (5):
+#         BUILD
+#         Error::Pure:
+#             err
+#         Mo::utils:
+#             check_array_object, check_number
+#         Wikibase::Datatype::Utils:
+#             check_entity
+#     private methods (0)
 #     internals: {
-#         grammatical_features   [
-#             [0] Wikibase::Datatype::Value::Item,
-#             [1] Wikibase::Datatype::Value::Item
+#         forms              [
+#             [0] Wikibase::Datatype::Form
 #         ],
-#         id                     undef,
-#         representations        [
-#             [0] Wikibase::Datatype::Value::Monolingual,
-#             [1] Wikibase::Datatype::Value::Monolingual
+#         id                 "L469",
+#         language           "Q9056",
+#         lastrevid          1428556087,
+#         lemmas             [
+#             [0] Wikibase::Datatype::Value::Monolingual
 #         ],
-#         statements             [
+#         lexical_category   "Q1084",
+#         modified           "2022-06-24T12:42:10Z" (dualvar: 2022),
+#         ns                 146,
+#         page_id            54393954,
+#         senses             [
+#             [0] Wikibase::Datatype::Sense
+#         ],
+#         statements         [
 #             [0] Wikibase::Datatype::Statement
-#         ]
+#         ],
+#         title              "Lexeme:L469"
 #     }
 # }
