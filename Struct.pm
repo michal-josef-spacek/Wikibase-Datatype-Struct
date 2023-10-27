6d92a6333de9125961f60cd0cddf5c604f3d0a02
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-10-27 23:16:20 +0200

    Prepare for 0.13 version

--- Struct.pm
+++ Struct.pm
@@ -3,7 +3,7 @@ package Wikibase::Datatype::Struct;
 use strict;
 use warnings;
 
-our $VERSION = 0.12;
+our $VERSION = 0.13;
 
 1;
 
@@ -134,6 +134,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.12
+0.13
 
 =cut
